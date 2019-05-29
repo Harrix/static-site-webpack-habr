33513c8f9c48b7b3db1a34942ba93a52c4e8267f
ekam230 <ekam230@gmail.com>
2019-05-29 05:40:52 +0400

    Update index.js
    For correct work jquery

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,10 @@
-import jQuery from "jquery";
+let $ = require('jquery');
+window.$ = window.jQuery = $;
+
 import popper from "popper.js";
 import bootstrap from "bootstrap";
+
+
 
 jQuery(function() {
   jQuery("body").css("color", "blue");
