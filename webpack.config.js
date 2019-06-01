624bf1ceeaca088b9d5e8afeeed93dd86857db53
Sergienko Anton <sergienkoanton@mail.ru>
2019-06-02 00:32:47 +0700

    Fix using of CopyWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const fs = require("fs");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
