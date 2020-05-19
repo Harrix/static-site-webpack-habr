f4e01b8e1c19d1acbc9e4cc4a41a4b6d570928c6
Sergienko Anton <sergienkoanton@mail.ru>
2020-05-19 10:53:03 +0300

    Update copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
 const path = require("path");
 const fs = require("fs");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
 function generateHtmlPlugins(templateDir) {
   const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  return templateFiles.map(item => {
+  return templateFiles.map((item) => {
     const parts = item.split(".");
     const name = parts[0];
     const extension = parts[1];
     return new HtmlWebpackPlugin({
       filename: `${name}.html`,
       template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`),
-      inject: false
+      inject: false,
     });
   });
 }
@@ -25,7 +25,7 @@ const htmlPlugins = generateHtmlPlugins("./src/html/views");
 const config = {
   entry: ["./src/js/index.js", "./src/scss/style.scss"],
   output: {
-    filename: "./js/bundle.js"
+    filename: "./js/bundle.js",
   },
   devtool: "source-map",
   mode: "production",
@@ -33,9 +33,9 @@ const config = {
     minimizer: [
       new TerserPlugin({
         sourceMap: true,
-        extractComments: true
-      })
-    ]
+        extractComments: true,
+      }),
+    ],
   },
   module: {
     rules: [
@@ -45,14 +45,14 @@ const config = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            options: {}
+            options: {},
           },
           {
             loader: "css-loader",
             options: {
               sourceMap: true,
-              url: false
-            }
+              url: false,
+            },
           },
           {
             loader: "postcss-loader",
@@ -65,52 +65,54 @@ const config = {
                     "default",
                     {
                       discardComments: {
-                        removeAll: true
-                      }
-                    }
-                  ]
-                })
-              ]
-            }
+                        removeAll: true,
+                      },
+                    },
+                  ],
+                }),
+              ],
+            },
           },
           {
             loader: "sass-loader",
             options: {
-              sourceMap: true
-            }
-          }
-        ]
+              sourceMap: true,
+            },
+          },
+        ],
       },
       {
         test: /\.html$/,
         include: path.resolve(__dirname, "src/html/includes"),
-        use: ["raw-loader"]
-      }
-    ]
+        use: ["raw-loader"],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "./css/style.bundle.css"
+      filename: "./css/style.bundle.css",
     }),
-    new CopyWebpackPlugin([
-      {
-        from: "./src/fonts",
-        to: "./fonts"
-      },
-      {
-        from: "./src/favicon",
-        to: "./favicon"
-      },
-      {
-        from: "./src/img",
-        to: "./img"
-      },
-      {
-        from: "./src/uploads",
-        to: "./uploads"
-      }
-    ])
-  ].concat(htmlPlugins)
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./src/fonts",
+          to: "./fonts",
+        },
+        {
+          from: "./src/favicon",
+          to: "./favicon",
+        },
+        {
+          from: "./src/img",
+          to: "./img",
+        },
+        {
+          from: "./src/uploads",
+          to: "./uploads",
+        },
+      ],
+    }),
+  ].concat(htmlPlugins),
 };
 
 module.exports = (env, argv) => {
