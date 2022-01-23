b17d18154c052720da00725e9ba5da05b56b1ede
Sergienko Anton <sergienkoanton@mail.ru>
2022-01-24 01:08:26 +0300

    Use dart-sass instead node-sass

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,7 @@ const config = {
           {
             loader: "sass-loader",
             options: {
+              implementation: require("sass"),
               sourceMap: true,
             },
           },
